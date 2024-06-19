adeeb5876253540b37aae1a192d97ca7ac686107
Aidan <am.mcdonald00@gmail.com>
2024-06-19 21:49:01 +0800

    Build out portfolio skeleton section

--- app/(root)/portfolio/page.tsx
+++ app/(root)/portfolio/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import React from "react";
-import "./Portfolio.css"
+import Image from "next/image";
+import "./Portfolio.css";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { MaterialSymbolsCircle } from "../../../components/ui/Icons";
 
 const page = () => {
 	// on page load
@@ -53,6 +55,7 @@ const page = () => {
 			);
 	}, []);
 
+	// on scroll
 	useGSAP(() => {
 		gsap.registerPlugin(ScrollTrigger);
 
@@ -106,14 +109,90 @@ const page = () => {
 				},
 				"<",
 			)
-			.fromTo("#view-projects-text", {
-				opacity: 0,
-				y: 40, },{
-				opacity: 1,
-				y: 0,
-				duration: 3,
-			}, +5)
+			.fromTo(
+				"#view-projects-text",
+				{
+					opacity: 0,
+					y: 40,
+				},
+				{
+					opacity: 1,
+					y: 0,
+					duration: 3,
+				},
+				+5,
+			)
+			.fromTo(
+				"#project-display",
+				{
+					opacity: 0,
+					y: 70,
+				},
+				{
+					y: 0,
+					opacity: 1,
+					duration: 5,
+					ease: "power2",
+				},
+				"<",
+			)
+			.fromTo(
+				".project-selection-btn",
+				{
+					x: -150,
+					opacity: 0,
+				},
+				{
+					x: 0,
+					opacity: 1,
+					duration: 3,
+					stagger: 0.6,
+				},
+				"<",
+			);
 	});
+
+	// Add hover effect to #project-screen
+	function handleMouseEnter(e: any) {
+		if (e.target.id === "project-screen") {
+			gsap.fromTo(
+				`#project-screen`,
+				{
+					background:
+						"radial-gradient(circle at bottom right, #242630e8 0%, #242630e8 100%)",
+				},
+				{
+					background:
+						"radial-gradient(circle at bottom right, #45071e 0%, #242630 100%)",
+					duration: 0.5,
+				},
+			);
+		}
+		if (e.target.id === "project-text") {
+			gsap.fromTo(
+				`#project-text`,
+				{
+					background:
+						"radial-gradient(circle at bottom right, #242630e8 0%, #242630e8 100%)",
+				},
+				{
+					background:
+						"radial-gradient(circle at bottom right, #45071e 0%, #242630 100%)",
+					duration: 0.5,
+				},
+			);
+		}
+	}
+
+	function handleMouseLeave(e: any) {
+		if (e.target.id) {
+			gsap.to(`#${e.target.id}`, {
+				background:
+					"radial-gradient(circle at bottom right, #242630e8 0%, #242630e8 100%)",
+				duration: 0.5,
+			});
+		}
+	}
 
 	return (
 		<div id="animation-id">
@@ -148,7 +227,10 @@ const page = () => {
 
 				<div className=" flex flex-col leading-9 justify-center items-center ">
 					{/* View my projects text */}
-					<div className="text-2xl font-[400] absolute top-[15%] opacity-0" id="view-projects-text">
+					<div
+						className="text-2xl font-[400] absolute top-[15%] opacity-0"
+						id="view-projects-text"
+					>
 						<span className="">
 							<span className="italic text-[#dcaa7e] font-medium">Click</span>{" "}
 							or{" "}
@@ -159,24 +241,88 @@ const page = () => {
 				</div>
 
 				{/* Portfolio projects */}
-				<div className="flex flex-row h-screen mt-10" id="portfolio-projects-section">
-        
-        <div id="project-selection-btns" className="flex flex-col justify-center items-start gap-10 pl-10">
-            <button className="project-selection-btn text-white border border-white rounded px-4 py-2">Project 1</button>
-            <button className="project-selection-btn text-white border border-white rounded px-4 py-2">Project 2</button>
-            <button className="project-selection-btn text-white border border-white rounded px-4 py-2">Project 3</button>
-            <button className="project-selection-btn text-white border border-white rounded px-4 py-2">Project 4</button>
-        </div> 
-        
-        <div className="flex grow justify-center items-center gap-10 px-10" id="project-display">
-            <div id="project-screen" className="w-1/2 h-[60%] rounded border border-[#363c53] bg-[#2a2d38c9] flex justify-center items-center text-white">
-                <div>Project Screen</div>
-            </div>
-            <div id="project-text" className="w-1/4 h-[60%] rounded border border-[#363c53] bg-[#2a2d38c9] flex justify-center items-center text-white">
-                <div>Project Text</div>
-            </div>
-        </div>
-		</div> 
+				<div
+					className="flex flex-row h-screen mt-10"
+					id="portfolio-projects-section"
+				>
+					{/* TODO: fix text wrap in buttons */}
+					<div
+						id="project-selection-btns"
+						className="flex flex-col justify-center items-start gap-10 pl-10"
+					>
+						<button className="project-selection-btn text-white border border-white rounded px-4 py-2 opacity-0">
+							Project 1
+						</button>
+						<button className="project-selection-btn text-white border border-white rounded px-4 py-2 opacity-0">
+							Project 2
+						</button>
+						<button className="project-selection-btn text-white border border-white rounded px-4 py-2 opacity-0">
+							Project 3
+						</button>
+						<button className="project-selection-btn text-white border border-white rounded px-4 py-2 opacity-0">
+							Project 4
+						</button>
+					</div>
+
+					<div
+						className="flex grow justify-center items-center gap-14 pr-10 opacity-0"
+						id="project-display"
+					>
+						{/* Project Preview */}
+						<div
+							id="project-screen"
+							onMouseEnter={handleMouseEnter}
+							onMouseLeave={handleMouseLeave}
+							className="w-1/2 h-[60%] rounded-xl border-2 border-[#343849] flex flex-col justify-center items-center text-white"
+						>
+							<div id="project-contents" className="w-full p-8 flex flex-col justify-between h-full">
+								<div
+									id="project-top-section"
+									className="flex flex-row justify-center items-center gap-4"
+								>
+									<div id="three-dots" className="flex flex-row gap-2">
+										<MaterialSymbolsCircle height={"1.2em"} width={"1.2em"} />
+										<MaterialSymbolsCircle height={"1.2em"} width={"1.2em"} />
+										<MaterialSymbolsCircle height={"1.2em"} width={"1.2em"} />
+									</div>
+
+									<div
+										id="search-bar"
+										className="border-2 rounded-full border-[#fff] w-full flex justify-end pr-3"
+									>
+										<span>x</span>
+									</div>
+								</div>
+								<div
+									id="project-img-container"
+									className="h-full border-2 border-[#fff] mt-7"
+								>
+									<img
+										src="/projects/Stride-for-Education.png"
+										alt="Logo"
+										className=""
+										id="project-img"
+									/>
+								</div>
+							</div>
+						</div>
+						{/* Project Description */}
+						<div
+							id="project-text"
+							onMouseEnter={handleMouseEnter}
+							onMouseLeave={handleMouseLeave}
+							className="w-1/4 h-[60%] rounded-xl border-2 border-[#343849] flex justify-center items-center text-white"
+						>
+							<div id="project-description" className="m-8">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+								Doloremque dicta ipsam blanditiis doloribus, culpa sequi labore,
+								veritatis laboriosam esse enim maxime earum, maiores dignissimos
+								ad facilis debitis necessitatibus assumenda nam nostrum vitae
+								eaque minus sint tenetur laborum! Iusto, esse labore.
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
